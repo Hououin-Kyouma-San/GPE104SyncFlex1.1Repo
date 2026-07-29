451f1a4fd5e73145c0b2a311b45e97cbae6c1ada
Hououin-Kyouma-San <162522705+Hououin-Kyouma-San@users.noreply.github.com>
2026-07-29 02:04:28 -0700

    Added Hello World Component
    - Created "HelloWorld.cs" script
    - Created "Empty" GameObject
    - Attached "HelloWorld.cs" to "Empty" as a component

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Helo World!!! I'm Late!!!");
+    }
+
+    // I still can't record audio, but IT installed the software I need to work
+    // Update is called once per frame
+    void Update()
+    {
+    //    Debug.Log("Helo World!!! I'm Late!!!"); This will repeat in the console!!!
+    }
+}
