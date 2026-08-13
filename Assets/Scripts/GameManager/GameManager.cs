2cc35280195489ee7a7b4a5836a60bf88ea48775
Hououin-Kyouma-San <162522705+Hououin-Kyouma-San@users.noreply.github.com>
2026-08-12 20:05:36 -0700

    Updated Game Manager
    - Added UI elements to scene and set up base functionality
         - Updated "GameManager.cs" to toggle active state of UI
              - Added "TitleScreen" GameObject
              - Added "MainMenu" GameObject
              - Added "OptionsScreen" GameObject
              - Added "Credits" GameObject
              - Added "Gameplay" GameObject
              - Added "GameOverScreen" GameObject

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -12,6 +12,15 @@ public class GameManager : MonoBehaviour
 
     // Keeps track of list of Meteors
     public List<DeathMeteor> meteors;
+
+    // Game States
+    public GameObject TitleScreenStateObject;
+    public GameObject MainMenuStateObject;
+    public GameObject OptionsScreenStateObject;
+    public GameObject CreditsScreenStateObject;
+    public GameObject GameplayStateObject;
+    public GameObject GameOverStateObject;
+
     private void Awake()
     {
         meteors = new List<DeathMeteor>();
@@ -30,7 +39,7 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        ActivateTitleScreen();
     }
 
     // Update is called once per frame
@@ -44,5 +53,68 @@ public class GameManager : MonoBehaviour
         {
             Debug.Log("Victory");
         }
+    }
+
+    // Deactivates all screen objects
+    private void DeactivateAllStates()
+    {
+        TitleScreenStateObject.SetActive(false);
+        MainMenuStateObject.SetActive(false);
+        OptionsScreenStateObject.SetActive(false);
+        CreditsScreenStateObject.SetActive(false);
+        GameplayStateObject.SetActive(false);
+        GameOverStateObject.SetActive(false);
+    }
+
+    // Activates title screen
+    public void ActivateTitleScreen()
+    {
+        DeactivateAllStates();
+
+        TitleScreenStateObject.SetActive(true);
+    }
+
+    // Activates main menu screen
+    public void ActivateMainMenuScreen()
+    {
+        DeactivateAllStates();
+
+        MainMenuStateObject.SetActive(true);
+    }
+
+    // Activates options screen
+    public void ActivateOptionsScreen()
+    {
+        DeactivateAllStates();
+
+        OptionsScreenStateObject.SetActive(true);
+    }
+
+    // Activates credits screen
+    public void ActivateCreditsScreen()
+    {
+        DeactivateAllStates();
+
+        CreditsScreenStateObject.SetActive(true);
+    }
+
+    // Activates Gameplay screen
+    public void ActivateGameplayScreen()
+    {
+        DeactivateAllStates();
+
+        GameplayStateObject.SetActive(true);
+
+        // Do anything else to get our game to run
+
+        // TODO: Return to implement fully
+    }
+
+    // Activates game over screen
+    public void ActivateGameOverScreen()
+    {
+        DeactivateAllStates();
+
+        GameOverStateObject.SetActive(true);
     }
 }
