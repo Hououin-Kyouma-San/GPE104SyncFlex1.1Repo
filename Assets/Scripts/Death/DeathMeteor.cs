6dc4e5078bf2222de20c3dba38b0deb51db702ad
Hououin-Kyouma-San <162522705+Hououin-Kyouma-San@users.noreply.github.com>
2026-09-04 03:49:02 -0700

    Updated Death Meteor Script and Prefabs
    - Updated death meteor script and adjusted prefabs
         - Reworked "DeathMeteor.cs" to add fragmenting mechanic to meteors on death
         - Changed "damageDelay" variables to improve timing
         - Attached "Meteor.cs" script to "Meteor" prefab

--- Assets/Scripts/Death/DeathMeteor.cs
+++ Assets/Scripts/Death/DeathMeteor.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class DeathMeteor : Death
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         if (GameManager.instance.meteors != null)
@@ -10,20 +9,34 @@ public class DeathMeteor : Death
             GameManager.instance.meteors.Add(this);
         }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public override void Die()
     {
         if (GameManager.instance.meteors != null)
         {
             GameManager.instance.meteors.Remove(this);
-        }
 
+            // Checks if half a meteor's size is greater than the minSize, if not, it's too small to split
+            if ((this.GetComponent<Meteor>().size * 0.5f) > this.GetComponent<Meteor>().minSize)
+            {
+                // If large enough, the meteor splits in two smaller parts
+                BreakApart();
+                BreakApart();
+            }
+        }
         Destroy(gameObject);
+    }
+    private void BreakApart()
+    {
+        // Copies the current meteor position and offsets new position by a random amount within a small area
+        Vector2 position = this.transform.position;
+        position += Random.insideUnitCircle * 0.5f;
+
+        // Creates two smaller copies of the meteor and sets a random trajectory
+        Meteor half = Instantiate(this.GetComponent<Meteor>(), position, this.GetComponent<Meteor>().transform.rotation);
+        half.GetComponent<Meteor>().size = this.GetComponent<Meteor>().size * 0.5f;
+        half.SetTrajectory(Random.insideUnitCircle.normalized);
+
+        // Gets health of meteor copies and multiplies by 1.25, giving them a slight buff
+        half.GetComponent<Meteor>().GetComponent<Health>().currentHealth = GetComponent<Health>().maxHealth * 1.25f;
     }
 }
